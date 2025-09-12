469f4789a5f71620e5d282775c89526393bf0782
ErtugrulArae <aracertugrul407@gmail.com>
2025-09-12 16:04:41 +0300

    hakkimda demo deneme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ import Footer from "@/components/footer/FooterCreatic"
 import Blog from "@/components/blog/blog"
 import Sss from "@/components/sss/app"
 import Mockup from "@/components/mockup/index"
+import Arka from "@/components/arkaplanelektrik/app"
 
 
 export default function Page() {
@@ -17,6 +18,7 @@ export default function Page() {
     <main>
 
       <Main />
+      {/* <Arka /> */}
       <Grafik />
       <YönlendirmeSeo />
 
---
+++ src/components/arkaplanelektrik/app.tsx
@@ -0,0 +1,18 @@
+
+"use client";
+
+import Lightning from "./Lightning";
+
+export default function LightningWrapper() {
+  return (
+    <div style={{ width: "100%", height: "600px", position: "relative" }}>
+      <Lightning
+        hue={220}
+        xOffset={0}
+        speed={1}
+        intensity={1}
+        size={1}
+      />
+    </div>
+  );
+}
